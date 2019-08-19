7f4b306d58df02ddbefa999801891dea672e46a8
Mayank Raj <35501314+Mayankraj23@users.noreply.github.com>
2019-08-20 00:51:17 +0530

    Update routes

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const routes = require("next-routes")();
 
 routes
   .add("details", "/details/:aadhaar")
-  .add("candidatedetails", "/candidatedetails/:ward");
+  .add("candidatedetails", "/candidatedetails/:ward")
+  .add("result", "/result/:ward");
 
 module.exports = routes;
