08f9571e47a5e2ec349a35ea44d471d74a342a08
Geetesh Gupta <gupta.15@iitj.ac.in>
2019-08-20 17:58:43 +0530

    Include candidate in routes

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const routes = require("next-routes")();
 
 routes
   .add("details", "/details/:aadhaar")
+  .add("candidate", "/candidate/:aadhaar")
   .add("candidatedetails", "/candidatedetails/:ward")
   .add("result", "/result/:ward");
 
